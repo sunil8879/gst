c5b7e02752c5be31670796eee17ecbd5cf22bd31
sunil8879 <sunilvasarkar@yahoo.com>
2026-03-20 14:10:24 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'invoice-app-v1';
+const assetsToCache = [
+  'index.html',
+  'manifest.json'
+];
+
+// Install Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(assetsToCache);
+    })
+  );
+});
+
+// Fetch Assets from Cache
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
